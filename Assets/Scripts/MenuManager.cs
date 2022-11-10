77fe0947ae820095605a864a56a20f313ae2bae2
leonadX <achimugu24@gmail.com>
2022-11-10 11:43:52 +0100

    Scale with screen size

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -11,7 +11,7 @@ public class MenuManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        LeanTween.moveLocal(placeHolder, new Vector3(0, 0, 0), 1.5f).setEase(LeanTweenType.easeInOutBack);
+        LeanTween.moveLocal(placeHolder, new Vector3(0, 280, 0), 1.5f).setEase(LeanTweenType.easeInOutBack);
         LeanTween.moveLocal(play, new Vector3(0, play.GetComponent<RectTransform>().anchoredPosition.y, 0), 1.5f).setEase(LeanTweenType.easeInOutBack);
         LeanTween.moveLocal(quit, new Vector3(0, quit.GetComponent<RectTransform>().anchoredPosition.y, 0), 1.5f).setEase(LeanTweenType.easeInOutBack);
     }
