c0c65f3ff50bddac5ed9ce494e9f4630aad52c18
Ysabel Katchy <ykatchy@gmail.com>
2022-11-11 01:13:23 +0100

    Adjusted the game over menu and pause menu

--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -13,8 +13,8 @@ public class Stats : MonoBehaviour
     void Start()
     {
         defeated.text += GameManager.instance.score;
-        LeanTween.moveLocal(gameObject.transform.GetChild(0).gameObject, new Vector3(0, 0, 0), 1.5f).setEase(LeanTweenType.easeInOutBack);
-        LeanTween.scale(defeated.gameObject, new Vector3(1, 1, 1), .5f).setDelay(1.5f).setEase(LeanTweenType.easeOutBack);
+        LeanTween.moveLocal(gameObject.transform.GetChild(0).gameObject, new Vector3(0, -20, 0), 1.5f).setEase(LeanTweenType.easeInOutBack);
+        LeanTween.scale(defeated.gameObject, new Vector3(1, 1, 1), 1.5f).setDelay(1.5f).setEase(LeanTweenType.easeOutBack);
         LeanTween.scale(retry, new Vector3(1, 1, 1), .5f).setDelay(1.5f).setEase(LeanTweenType.easeOutBack);
         LeanTween.scale(home, new Vector3(1, 1, 1), .5f).setDelay(1.5f).setEase(LeanTweenType.easeOutBack);
     }
